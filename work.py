3831f4c70e52e32213c24acdbab29a37ee915ecb
LTDwang <1429168201@qq.com>
2021-09-25 11:51:37 +0800

    Ture

--- work.py
+++ work.py
@@ -1,5 +1,5 @@
-a =int (input(""))
-if a > 10:
-    print("abab")
-else:
-    print("cdcd") 
+True and True
+False and True
+False and True
+print (5 > 3 and 3 > 5 , 1 < 2 and 2 < 3 , 3 < 2 and 3 > 2)
+print ('\u4e2d\u6587')
